ec2d1a8723fe7559305a1141f4cfc5ac97360824
Karolis Davli <kdavlijevas@gmail.com>
2022-03-03 09:05:10 +0100

    new player form. Open form event

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var Gameboard = (function() {
   let gameOn = false;
-  board = ['x', 'x', 'x', 'o', 'o', 'o', 'x', 'x', 'x',];
+  board = ['x', 'x', 'x', 'o', 'o', 'o', 'x', 'x', 'x'];
+  const playerForm = document.querySelector('.player-form');
   const content = document.querySelector('.content');
   const gameBoard = document.createElement('div');
   gameBoard.classList.add('game-board');
   const start = document.querySelector('.start');
 
-
-
-  const gameInit = () => {
+  const renderBoard = () => {
     if (gameOn) {
       alert('Game already started');
     } else {
@@ -27,45 +26,36 @@ var Gameboard = (function() {
   }
 
 
+  const _toggleForm = () => {
+    if (playerForm.style.display === 'flex') {
+      playerForm.style.display = 'none';
+    } else {
+      playerForm.style.display = 'flex';
+    }
+  }
+
+  const gameInit = (e) => {
+    e.preventDefault();
+    let player1 = document.querySelector('#player1').value;
+    let player2 = document.querySelector('#player2').value;
+    console.log(`Player ${player1} and player ${player2} have joined the game`);
+    renderBoard();
+    _toggleForm();
+  }
+
+  start.addEventListener('click', _toggleForm);
+  playerForm.addEventListener('submit', gameInit);
+  
   const _takeTurn = (e, player) => {
     let currentSquareIndex = e.target.dataset.index;
     console.log(currentSquareIndex);
     e.target.style.backgroundColor = '#535353';
-
   }
 
-  start.addEventListener('click', gameInit)
-
-  const addPlayer = (name) => {
-    console.log(`Hello ${name}`);
-    _shitsPrivate(name);
-  }
-
-  const _shitsPrivate = (priv) => {
-    console.log(`This shit is private. Called by ${priv}`);
-  }
-
-  const newPlayer = (name) => {
-    let score = 0;
-    // const takeTurn = () => {
-    //   console.log(`Player ${name} made a move`);
-    //   score++;
-    //   console.log(score);
-    // }
-  
-    return {
-      // takeTurn,
-    }
-  }
-
-  return {
-    addPlayer,
-    newPlayer,
-  }
-})()
 
 
-// const tom = Gameboard.newPlayer('tom');
-// tom.takeTurn();
-// const mary = Gameboard.newPlayer('mary');
+
+})();
+
+
 
