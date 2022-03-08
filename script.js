5a3d51f1d48c11d1bfac8e3f1ac861475aef9f84
Karolis Davli <kdavlijevas@gmail.com>
2022-03-08 08:29:27 +0100

    code structure

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-var Gameboard = (function() {
+const createPlayer = (name, marker) => {
+  return {name, marker};
+}
+
+const gameboard = (() => {
   let gameOn = false;
-  board = ['x', 'x', 'x', 'o', 'o', 'o', 'x', 'x', 'x'];
+  board = ['', '', '', '', '', '', '', '', '',];
   const playerForm = document.querySelector('.player-form');
   const content = document.querySelector('.content');
   const gameBoard = document.createElement('div');
   gameBoard.classList.add('game-board');
   const start = document.querySelector('.start');
 
+  const addClickEvents = () => {
+    let i = 0;
+    board.forEach(square => {
+      square = document.createElement('div');
+      square.setAttribute('data-index', i);
+      square.classList.add('square');
+      i++;
+      gameBoard.append(square);
+      square.addEventListener('click', takeTurn);
+    });
+  }
+
   const renderBoard = () => {
     if (gameOn) {
       alert('Game already started');
     } else {
       gameOn = true;
-      let i = 0;
-      board.forEach(square => {
-        square = document.createElement('div');
-        square.setAttribute('data-index', i);
-        square.classList.add('square');
-        i++;
-        gameBoard.append(square);
-        square.addEventListener('click', _takeTurn);
-      });
+      addClickEvents();
     }
     content.append(gameBoard);
   }
-
 
   const _toggleForm = () => {
     if (playerForm.style.display === 'flex') {
@@ -34,11 +41,19 @@ var Gameboard = (function() {
     }
   }
 
+    // let playerOneName = document.querySelector('#player1').value;
+    // let playerTwoName = document.querySelector('#player2').value;
+    
+  const takeTurn = (e, playerTwo) => {
+    let currentSquareIndex = e.target.dataset.index;
+    console.log(currentSquareIndex);
+    e.target.style.backgroundColor = '#535353';
+    board[currentSquareIndex] = playerTwo;
+    console.log(board);
+  }
+
   const gameInit = (e) => {
     e.preventDefault();
-    let player1 = document.querySelector('#player1').value;
-    let player2 = document.querySelector('#player2').value;
-    console.log(`Player ${player1} and player ${player2} have joined the game`);
     renderBoard();
     _toggleForm();
   }
@@ -46,15 +61,6 @@ var Gameboard = (function() {
   start.addEventListener('click', _toggleForm);
   playerForm.addEventListener('submit', gameInit);
   
-  const _takeTurn = (e, player) => {
-    let currentSquareIndex = e.target.dataset.index;
-    console.log(currentSquareIndex);
-    e.target.style.backgroundColor = '#535353';
-  }
-
-
-
-
 })();
 
 
