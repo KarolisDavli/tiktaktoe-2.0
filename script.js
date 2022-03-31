114a777746d235f2125476a7d6da67cbf1acb19a
Karolis Davli <kdavlijevas@gmail.com>
2022-03-31 10:42:29 +0300

    Correct game start msg. Get game status

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const gameArea = (() => {
       alert('Game already started');
     } else {
       gameOn = true;
-      game.updateTurnDisplay(`Player 1 starts`);
+      game.updateTurnDisplay(`Next to move: Player 1`);
       _addClickEvents();
       _renderGameResetBtn();
     }
@@ -56,6 +56,8 @@ const gameArea = (() => {
 
 const game = (() => {
 
+  let gg = false;
+
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -69,7 +71,6 @@ const game = (() => {
 
   let playerOne = createPlayer('Player 1', 'x');
   let playerTwo = createPlayer('Player 2', 'o');
-
   let activePlayer = playerOne;
   let activePlayerDisplay = document.querySelector('.active-player-display');
 
@@ -85,21 +86,18 @@ const game = (() => {
 
   const _placeActiveSymbol = (e) => {
     activePlayer == playerOne ? e.target.style.backgroundImage = "url('img/brush-solid.svg')" : e.target.style.backgroundImage = "url('img/ghost-solid.svg')";
-    
   }
 
   const takeTurn = (e) => {
     let currentSquareIndex = e.target.dataset.index;
 
-    if (gameArea.board[currentSquareIndex] == '') {
+    if (gameArea.board[currentSquareIndex] == '' && gg === false) {
       gameArea.board[currentSquareIndex] = activePlayer.marker;
-      updateTurnDisplay(`Next to move: ${activePlayer.name}`);
-      _placeActiveSymbol(e);
-      _checkWinCondition();
       _getNextPlayer();
+      updateTurnDisplay(`Next to move: ${activePlayer.name}`);
+      _checkWinCondition();
+      _placeActiveSymbol(e);
       _checkIfTie();
-    } else {
-      alert('Choose other tile')
     }
   }
 
@@ -117,18 +115,22 @@ const game = (() => {
       const a = condition[0];
       const b = condition[1];
       const c = condition[2];
-
-      return (gameArea.board[a] && gameArea.board[a] === gameArea.board[b] && gameArea.board[a] === gameArea.board[c]);
+      return gameArea.board[a] && gameArea.board[a] === gameArea.board[b] && gameArea.board[a] === gameArea.board[c];
     });
+
 
     if (win) {
       updateTurnDisplay(`Game over: ${activePlayer.name} wins.
       Reset Game to play again`);
+      gg = true;
     }
   }
 
+  
+
   const resetGame = () => {
     gameArea.board = ['', '', '', '', '', '', '', '', '',];
+    gg = false;
     activePlayer = playerOne;
     updateTurnDisplay(`Next to move: ${activePlayer.name}`);
     let elemPictures = document.querySelectorAll('.square');
